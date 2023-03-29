0bb1aa2d0f234569b0cc3e31dab2040655415190
divyavijayakumar72 <63861781+divyavijayakumar72@users.noreply.github.com>
2023-03-28 20:22:08 -0700

    bfs

--- src/main/java/edu.asu.scai.rise/Main.java
+++ src/main/java/edu.asu.scai.rise/Main.java
@@ -36,9 +36,9 @@ public class Main {
         graph.outputGraphics("response.png", "png");
         /* FEATURE 4 */
 
-        /* PART 2- STEP 3 - BFS */
+        /* PART 2- STEP 3 a - BFS */
         graph.GraphSearch("a","d");
-        /* PART 2 - STEP 3 - BFS */
+        /* PART 2 - STEP 3 a - BFS */
 
     }
 }
